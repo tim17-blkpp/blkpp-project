a0abe065190cc38b292580a0f00b5641975bf431
farhanhnfx <farhanhanif@mail.ugm.ac.id>
2023-12-28 14:05:47 +0700

    fix bug tables not found on composer install/update

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use App\Models\KategoriBlogModel;
 use App\Models\KategoriLowonganKerjaModel;
 use App\Models\KategoriPelatihanModel;
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Pagination\Paginator;
 use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
@@ -28,16 +29,18 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        Paginator::useBootstrap();
-        $kategori_pelatihan = KategoriPelatihanModel::orderBy('nama', 'ASC')->get();
-        $kategori_lowongan_kerja = KategoriLowonganKerjaModel::orderBy('nama', 'ASC')->get();
-        $kategori_blog = KategoriBlogModel::orderBy('nama', 'ASC')->get();
-        $data = array(
-            'kategori_pelatihan' => $kategori_pelatihan,
-            'kategori_lowongan_kerja' => $kategori_lowongan_kerja,
-            'kategori_blog' => $kategori_blog,
-        );
+        if (Schema::hasTable('kategori_pelatihan') && Schema::hasTable('kategori_lowongan_kerja') && Schema::hasTable('kategori_blog')) {
+            Paginator::useBootstrap();
+            $kategori_pelatihan = KategoriPelatihanModel::orderBy('nama', 'ASC')->get();
+            $kategori_lowongan_kerja = KategoriLowonganKerjaModel::orderBy('nama', 'ASC')->get();
+            $kategori_blog = KategoriBlogModel::orderBy('nama', 'ASC')->get();
+            $data = array(
+                'kategori_pelatihan' => $kategori_pelatihan,
+                'kategori_lowongan_kerja' => $kategori_lowongan_kerja,
+                'kategori_blog' => $kategori_blog,
+            );
 
-        View::share("service", $data);
+            View::share("service", $data);
+        }
     }
 }
